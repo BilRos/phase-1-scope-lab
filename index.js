f8e6bb50886029ddabdd820077ccb49b9a96f111
BilRos <billyrossowuor@gmail.com>
2024-03-25 16:43:52 +0300

    I got to manipulate some string

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+//modifies the customerName variable
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+
+//setBestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    console.log(bestCustomer)
+}
+
+
+//overwrites the best customer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    console.log(bestCustomer);
+}
+
+
+//unsuccessfully tries to reassign the least favorite customer
+const leastFavouriteCustomer = 'Dennis';
+
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = 'Jane'
+    console.log(leastFavouriteCustomer);
+}
+changeLeastFavoriteCustomer()
